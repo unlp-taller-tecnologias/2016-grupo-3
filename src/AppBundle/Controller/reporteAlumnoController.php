2fa5e19030adda00404ef96ae5cb7b4e37ba990e
Juan Marinelli <juanmarinelli10@gmail.com>
2016-11-28 12:38:40 -0300

    reporteAlumno

--- src/AppBundle/Controller/reporteAlumnoController.php
+++ src/AppBundle/Controller/reporteAlumnoController.php
@@ -24,9 +24,99 @@ class reporteAlumnoController extends Controller
     public function indexAction()
     {
         
+         $em = $this->getDoctrine()->getManager();
 
 
-        return $this->render('reporteAlumno/index.html.twig');
+         //DATOS DEL ALUMNO
+         $alumno = $em->getRepository('AppBundle:Alumnos')->findOneById($_GET['idAlumno']);
+        // var_dump($alumno->getId());
+        $comision = $em->getRepository('AppBundle:Comisiones')->findOneById($_GET['idComision']);
+        //OTRAS COMISIONES
+      //  $otras_comisiones=$alumno->getComisiones();
+       // var_dump($otras_comisiones);
+
+         //LASES DEL ALUMNO
+         $titulo=$alumno->getApellido();
+         $titulo .=", ".$alumno->getNombre();
+         $cursos = $em->getRepository('AppBundle:Cursos')->findOneById($_GET['idCursada']);
+         $clases = $cursos->getClases();
+         $cant_presentes="";
+         $cant_clases_tomadas=""; 
+          foreach ($clases as $clas) {
+                    if ($clas->getEstado() == "finalizada") {
+                      if ($clas->getRequerida() == 1) {
+                         $cant_clases_tomadas=$cant_clases_tomadas + 1;
+                      }
+                     
+                    }
+                   $asistencia = encontrarUnaAsistenciaByAlumnoAndClase($em,$alumno->getId(),$clas->getId());
+                    if ($asistencia == null) {
+                        
+                   } else { 
+                    if ($asistencia[0]->getEstado() == "presente" or $asistencia[0]->getEstado() == "justificado") {
+                      $cant_presentes=$cant_presentes + 1;
+                    }
+                    
+                   }
+
+                }
+                $clases_alumno= array("clases"=>$clases,"cant_clases_tomadas"=>$cant_clases_tomadas,"cant_presentes"=>$cant_presentes);
+
+         //NOTAS DEL ALUMNO
+            $cursos = $em->getRepository('AppBundle:Cursos')->findOneById($_GET['idCursada']);
+            $parciales = $cursos->getParciales();
+
+          foreach ($parciales as $par) {
+                  $parcial = $em->getRepository('AppBundle:Parcial')->findOneById($par->getId());
+                  $instancias= $parcial->getInstancias();
+                foreach ($instancias as $instancia) {
+                   $array_inst [] = array('inst'=>$instancia->getNombre());
+                }
+
+                $array_parcial_instancia [] =array('parcial'=>$par,'instancia'=>$array_inst);
+                $array_inst="";
+             }
+
+                  foreach ($parciales as $par) {
+                    //recorro todos los parciales y voy trayendolos por el id
+                      $parcial = $em->getRepository('AppBundle:Parcial')->findOneById($par->getId());
+                      $instancias= $parcial->getInstancias();
+                   foreach ($instancias as $instancia) {
+                        //recorro todas las instancias de un parcial
+                          $nota = encontrarUnaNotaByAlumnoAndInstancia($em,$alumno->getId(),$instancia->getId());
+                          if ($nota == null) {
+                              $notas[] = (array(
+                                          "instancia"=>$instancia,
+                                          "estado"=>"n/a",
+                                          "nota"=>"", 
+                                          "observacion"=> "",
+                                          "condicion"=>"n/a",
+                                          "idNota"=>"0" )
+                                          );
+                          } else { 
+                              $notas[] = (array(
+                                          "instancia"=>$instancia,
+                                          "nota"=>$nota[0]->getNota(),
+                                          "condicion"=>$nota[0]->getCondicion(),
+                                          "observacion"=>$nota[0]->getObservacion(),
+                                          "estado"=>$nota[0]->getEstado(),
+                                          "idNota"=>$nota[0]->getId())
+                                          );
+                          }
+                          //$instancia_parcial[] = (array('instancia'=>$instancia,'notas'=>$notas));
+                         // $notas="";
+                        //$instancia = $em->getRepository('AppBundle:InstanciaParcial')->findOneById($_GET['idInstanciaP']);
+                   }
+                   
+                   $parcial_alumno[] = (array('parcial'=>$par,'instancia_parcial'=>$notas));
+                   $notas="";
+                 
+                  }
+
+
+         //OTRAS CURSADAS DE LA MISMA CATEDRA EN LA QUE PARTICIPO
+
+         return $this->render('reporteAlumno/index.html.twig',array('titulo'=>$titulo,'alumno'=>$alumno,'comision'=>$comision,'clases_alumno'=>$clases_alumno,'parcial_alumno'=>$parcial_alumno,'array_parcial_instancia'=>$array_parcial_instancia));
     }
 
  
--- src/AppBundle/function.inc.php
+++ src/AppBundle/function.inc.php
@@ -63,7 +63,7 @@ function encontrarAlumnos($em,$nom,$id_curso)
      $nom='%'.$nom.'%';
      $sql = "SELECT * FROM alumnos 
         LEFT JOIN inscriptos ON (inscriptos.idAlumno = alumnos.id )
-        WHERE (alumnos.nombre LIKE :nom) AND (inscriptos.idCurso = :id_curso) ";
+        WHERE (alumnos.nombre LIKE :nom OR alumnos.apellido LIKE :nom ) AND inscriptos.idCurso = :id_curso ";
    $params=array('nom'=>$nom, 'id_curso'=>$id_curso);
     $stmt = $em->getConnection()->prepare($sql);
     $stmt->execute($params);
